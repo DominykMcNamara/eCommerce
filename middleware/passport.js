d2425b3d1dd834b12268e5f4bed723286a9920f8
DominykMcNamara <dominyksmith@gmail.com>
2023-01-17 18:57:11 -0500

    implement user serialization

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,5 +1,19 @@
 const passport = require("passport");
-const strategy = require('../utils/strategy')
-
+const strategy = require("../utils/strategy");
 
 passport.use(strategy);
+
+passport.serializeUser((user, cb) => {
+  process.nextTick(() => {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+    });
+  });
+});
+
+passport.deserializeUser((user, cb) => {
+  process.nextTick(() => {
+    return cb(null, user);
+  });
+});

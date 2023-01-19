3123a3bc66a6bc0e26325d6a76cd391c8e9deada
DominykMcNamara <dominyksmith@gmail.com>
2023-01-19 15:54:34 -0500

    fix err

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const corsOptions = require("./config/corsOptions");
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger-output.json');
 const PORT = process.env.PORT || 3500;
 const session = require("express-session");
 const passport = require("passport");
@@ -17,9 +15,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
 app.use(cors(corsOptions));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(morgan("dev"));
 app.use(
   session({

9f878b302a0024f845b6cafa3d8bd7c66c16e9f9
DominykMcNamara <dominyksmith@gmail.com>
2023-01-20 15:55:08 -0500

    correct err

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,8 @@ const passport = require('passport')
 require('../config/passport')
 const router = new Router()
 
-router.post('/', passport.authenticate('local', {
-    successRedirect: '/',
+router.post('/password', passport.authenticate('local', {
+    successRedirect: '/password',
     failureRedirect: '/register'
 }))
 

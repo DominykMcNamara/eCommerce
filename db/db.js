5937ed06dc4d6680d760096880b83cf7f9530b73
DominykMcNamara <dominyksmith@gmail.com>
2023-01-22 18:09:35 -0500

    db refractor

---
+++ db/db.js
@@ -0,0 +1,80 @@
+const pg = require("pg")(async () => {
+  const usersTable = `
+        CREATE TABLE IF NOT EXISTS Users (
+            id INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            email VARCHAR(100)  NOT NULL UNIQUE,
+            password VARCHAR(50) NOT NULL,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50)
+        );`;
+
+  const productsTable = `
+        CREATE TABLE IF NOT EXISTS Products (
+            id INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+            name VARCHAR(50) NOT NULL,
+            price BIGINT NOT NULL ,
+            description VARCHAR(100)
+        );`;
+
+  const ordersTable = `
+        CREATE TABLE IF NOT EXISTS Orders (
+            id INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            total BIGINT NOT NULL,
+            status VARCHAR(50) DEFAULT "Unpaid",
+            user_id INT NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES Users(id) 
+        );`;
+
+  const orderItemsTable = `
+        CREATE TABLE IF NOT EXISTS Order_items (
+            id INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            order_id INT NOT NULL,
+            quantity INT NOT NULL,
+            price BIGINT NOT NULL,
+            product_id INT NOT NULL,
+            name VARCHAR(50) NOT NULL,
+            description VARCHAR(100) NOT NULL,
+            FOREIGN KEY (order_id) REFERENCES Orders(id)
+        );`;
+
+  const cartsTable = `
+        CREATE TABLE IF NOT EXISTS Carts (
+            id INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            user_id INT NOT NULL,
+            FOREIGN KEY user_id REFERENCES Users(id)
+        );`;
+
+  const cartItemsTable = `
+        CREATE TABLE IF NOT EXISTS Cart_items (
+            id INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+            cart_id INT NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            product_id INT NOT NULL,
+            quantity INT NOT NULL,
+            FOREIGN KEY cart_id REFERENCES Carts(id),
+            FOREIGN KEY product_id REFERENCES Products(id)
+        );`;
+
+  try {
+    const db = new pg.Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    await db.connect();
+    await db.query(usersTable);
+    await db.query(productsTable);
+    await db.query(ordersTable);
+    await db.query(orderItemsTable);
+    await db.query(cartsTable);
+    await db.query(cartItemsTable);
+
+    await db.end();
+  } catch (err) {
+    console.log(err);
+  }
+})();
--- db/index.js
+++ db/index.js
@@ -2,12 +2,10 @@ const pg = require("pg");
 
 
 const dbConfig = {
-
- connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL
 };
 
 const pool = new pg.Pool(dbConfig);
-pool.on("error", (err) => console.log(err));
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback);

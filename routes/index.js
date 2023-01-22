672c7feccddcce2602dfd29c005a15ac40137535
DominykMcNamara <dominyksmith@gmail.com>
2023-01-22 14:00:02 -0500

    Logout route added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
    app.use('/register', require('./register'))
    app.use('/login', require('./login'))
+   app.use('/logout', require('./logout'))
    app.use('/users', require('./users'))
    app.use('/products', require('./products'))
 }

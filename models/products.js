a8ddc27cb584c9a66bf80bf66c13e222a7bd18b5
DominykMcNamara <dominyksmith@gmail.com>
2023-01-23 21:14:10 -0500

    Cleanup

---
+++ models/products.js
@@ -0,0 +1,6 @@
+const db = require("../db");
+const pgp = require("pg-promise")({ capSQL: true });
+
+module.exports = class ProductModel {
+    
+}
--- routes/carts.js
+++
@@ -1,6 +0,0 @@
-const Router = require("express-promise-router");
-const router = new Router();
-const cartController = require('../controllers/cartController')
-router.get('/', cartController.getAllUsersCarts)
-
-module.exports = router

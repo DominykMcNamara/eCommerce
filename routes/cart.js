bfd759c82cdb0df4312e13cebfe8fdfff6d6e3e3
DominykMcNamara <dominyksmith@gmail.com>
2023-01-27 16:33:50 -0500

    complete cart routes

---
+++ routes/cart.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const checkAuthenticated = require("../middleware/isAuthenticated");
+const CartService = require("../services/CartService");
+const CartServiceInstance = new CartService();
+
+module.exports = (app) => {
+  app.use("/carts", [checkAuthenticated], router);
+
+  router.get("/myCarts", async (req, res, next) => {
+    try {
+      const response = await CartServiceInstance.getManyByUserId({
+        id: req.user.id,
+      });
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.get("/:id", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const response = CartServiceInstance.getOneById({ id: id });
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.patch("/:id", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const data = req.body;
+      const response = await CartServiceInstance.update({ id: id, ...data });
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.delete("/:id", async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const response = CartServiceInstance.deleteOne({ id: id });
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post("/", async (req, res, next) => {
+    try {
+      const data = req.body;
+      const response = CartServiceInstance.createCart({
+        user_id: req.user.id,
+      });
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const userRouter = require("./user");
 const authRouter = require("./auth");
 const productRouter = require("./product")
 const orderRouter = require("./order")
+const cartRouter = require("./cart")
 module.exports = (app, passport) => {
   userRouter(app);
   authRouter(app, passport);
   productRouter(app)
   orderRouter(app)
+  cartRouter(app)
 };
